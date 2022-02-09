4db8a4eaae56b64edb7c25b594f6990aeda03063
CodyCarlock <cjoelc39@gmail.com>
2022-02-08 17:45:01 -0700

    Updated models, Json Settings, and startup

---
+++ Models/TasksContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mission06.Models;
+
+
+namespace Mission06.Models
+{
+	public class TasksContext: DbContext
+	{
+		//constructor
+		public TasksContext(DbContextOptions<TasksContext> options) : base(options)
+	{
+		//leave balnk for now
+	}
+
+	public DbSet<TaskResponse> Responses { get; set; }
+	public DbSet<Categories> Categories { get; set; }
+
+	//seeding database with categories and tasks
+	protected override void OnModelCreating(ModelBuilder mb)
+	{
+		mb.Entity<Categories>().HasData(
+			 new Categories { CategoryId = 1, CategoryName = "Home" },
+			 new Categories { CategoryId = 2, CategoryName = "School" },
+			 new Categories { CategoryId = 3, CategoryName = "Work" },
+			 new Categories { CategoryId = 4, CategoryName = "Church" }
+
+		    );
+
+              mb.Entity<TaskResponse>().HasData(
+
+                new TaskResponse
+                {
+                    TaskId = 1,
+                    Task = "Finish Mission6",
+                    DueDate = new DateTime(2022-02-09),
+                    Quadrant = 1,
+                    Completed = true,
+                    CategoryId = 2
+                },
+                new TaskResponse
+                {
+                    TaskId = 2,
+                    Task = "Date Night",
+                    DueDate = new DateTime(2022-02-12),
+                    Quadrant = 2,
+                    Completed = false,
+                    CategoryId = 1
+                },
+                new TaskResponse
+                {
+                    TaskId = 3,
+                    Task = "Apply for Internships",
+                    DueDate =  new DateTime(2022-02-12),
+                    Quadrant = 3,
+                    Completed = false,
+                    CategoryId = 3
+                },
+                new TaskResponse
+                {
+                    TaskId = 4,
+                    Task = "Put strap on scabard",
+                    Quadrant = 4,
+                    Completed = false,
+                    CategoryId = 4
+                }
+            );
+
+	}
+
+}
+}
