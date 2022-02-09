c14915fbf10819968931edeebbe3c93a4ecfa7e6
CodyCarlock <cjoelc39@gmail.com>
2022-02-08 17:45:01 -0700

    Updated models, Json Settings, and startup

---
+++ Models/Categories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission06.Models
+{
+	public class Categories
+	{
+		[Key]
+		[Required]
+		public int CategoryId { get; set; }
+		public string CategoryName { get; set; }
+	}
+}
---
+++ Models/TaskResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Mission06.Models;
+
+namespace Mission06.Models
+{
+	public class TaskResponse
+	{
+		[Key]
+		[Required]
+		public int TaskId { get; set; }
+		[Required]
+		public string Task { get; set; }
+		public DateTime DueDate { get; set; }
+		[Required]
+		public int Quadrant { get; set; }
+        public Boolean Completed { get; set; }
+		//foriegn key of category
+		public int CategoryId { get; set; }
+		public Categories Categories { get; set; }
+    }
+}
